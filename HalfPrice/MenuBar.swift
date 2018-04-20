22072746287b5a50dfe02743e9a37ac37bf73841
QueenaHuang <cindy119xyz@gmail.com>
2018-04-20 16:13:41 +1000

    Set up the MenuBar basically

--- HalfPrice/MenuBar.swift
+++ HalfPrice/MenuBar.swift
@@ -9,10 +9,13 @@
 import UIKit
 import SnapKit
 
+let cellWidth:CGFloat = 200.0
+
 class MenuBar: UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
     lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
         cv.backgroundColor = UIColor.blue
         cv.dataSource = self
@@ -21,17 +24,22 @@ class MenuBar: UIView, UICollectionViewDelegate, UICollectionViewDataSource, UIC
     }()
 
     let cellId = "cellId"
+    let tagNames = ["Hot", "Frozen", "Drinks", "Household", "Beauty", "Living", "International", "Dairy", "Bakery", "Meat", "Stationery", "Pet", "Alcohol"]
 
     override init(frame: CGRect) {
         super.init(frame: frame)
 
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
-        
+        collectionView.register(MenuCell.self, forCellWithReuseIdentifier: cellId)
+
         addSubview(collectionView)
 
         collectionView.snp.makeConstraints { (make) in
             make.edges.equalTo(self)
         }
+
+        let selectedIndexPath = IndexPath(item: 0, section: 0)
+
+        collectionView.selectItem(at: selectedIndexPath, animated: false, scrollPosition: .bottom)
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -39,15 +47,67 @@ class MenuBar: UIView, UICollectionViewDelegate, UICollectionViewDataSource, UIC
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        return tagNames.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-        cell.backgroundColor = UIColor.red
-        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! MenuCell
+
+        cell.titleLabel.text = tagNames[indexPath.row]
+
         return cell
     }
 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: frame.height)
+    }
 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+
+
+}
+
+
+class MenuCell: UICollectionViewCell {
+
+
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setupViews()
+
+    }
+
+    override var isHighlighted: Bool {
+        didSet {
+            titleLabel.textColor = isHighlighted ? UIColor.white : UIColor.red
+        }
+    }
+
+    override var isSelected: Bool {
+        didSet {
+            titleLabel.textColor = isSelected ? UIColor.white : UIColor.red
+        }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setupViews() {
+        addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { (make) in
+            make.edges.equalTo(self)
+        }
+    }
+
+    
 }
